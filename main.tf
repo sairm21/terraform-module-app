6d959007176e8a9a1120b19ee1f2cf38f9bd0643
sairam <sairm21@gmail.com>
2023-10-20 21:42:38 +0530

    adding target group resource

--- main.tf
+++ main.tf
@@ -73,11 +73,19 @@ resource "aws_launch_template" "app_launch_template" {
 
 }
 
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.component}-${var.env}-TG"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
 resource "aws_autoscaling_group" "app_ASG" {
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
 
   launch_template {
     id      = aws_launch_template.app_launch_template.id
